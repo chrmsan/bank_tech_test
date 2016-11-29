f79df2e5b22c06ade164e3dbfcbac3f870ba78f2
Christopher Sanchez <sanchez.christopherm@gmail.com>
2016-11-29 16:07:58 +0100

    Added test for deposit

--- lib/account.rb
+++ lib/account.rb
@@ -1,2 +1,9 @@
 class Account
+
+	attr_reader :balance, :logger
+
+	def initialize(balance = 0, logger = [])
+		@balance = balance
+		@logger = logger
+	end
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -2,4 +2,21 @@ require 'spec_helper'
 
 describe Account do
 
+	describe '#initialize' do
+		it 'has a balance of 0' do
+			expect(subject.balance).to eq(0)
+		end
+
+		it 'savings log is empty' do
+			expect(subject.logger).to be_empty
+		end
+	end
+
+	describe '#deposit' do
+		it 'can be deposited 500 in the account' do
+			amount = 500
+			expect {subject.deposit(amount)}.to change { subject.balance }.by amount
+		end
+	end
+
 end
